877b5c87b84929d65929be011ee86f547550623c
Rohish Zade <zaderohish5@gmail.com>
2024-07-21 00:31:20 +0530

    Python is awesome

--- Day_1/hello_world.py
+++
@@ -1,5 +0,0 @@
-print("Hello, World!")
-
-print("""Hello, My name is Rohish Zade. As a Data Engineer, My key responsibilities are to Extract, Transform, Load, and most importantly analyze the data in various formats.
-
-I am currently working in one of the MNC as a Data engineer in a project where migrating On-Prem ETL jobs to the cloud using tools like Azure Data Factory and Databricks, developing and maintaining data pipelines, transforming raw data for analysis, managing databases, and implementing automation techniques to streamline processes. I collaborate with teams, monitor system performance, document processes, and stay updated on industry trends to drive innovation and ensure efficient data management.""")
