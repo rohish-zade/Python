a5a3dbc53af43a4baa73d2207a97d8ad21e4ba16
Rohish Zade <zaderohish5@gmail.com>
2024-08-15 23:03:42 +0530

    test file

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-import os
-
-# os.makedirs(f'{success_files_path}', exist_ok=True)
-a = True
-b = False
-
-for num in range(1, 11):
-    if b:
-       continue
-    print(num)
-else:
-    print("No numbers after 4")
+# This is a test file
 
