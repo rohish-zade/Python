df91e0cdd906c4416e764327709d469066773f0e
Rohish Zade <zaderohish5@gmail.com>
2024-08-28 23:09:56 +0530

    ETL

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
 # This is a test file
 
 import pandas as pd
+import sqlalchemy as sal
 
-print(pd)
+connection_string = ('mssql://<sql server _name>\SQLEXPRESS/<database_name>?driver=ODBC+DRIVER+17+FOR+SQL+SERVER')
+engine = sal.create_engine(connection_string)
+conn=engine.connect()
+
+
+# reading from a table in sql server and storing in a df
+df_sql = pd.read_sql_query('select * from orders', conn)
+print(df_sql)
+
