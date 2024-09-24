67639b79e35742fba543a827491402cc8e79f620
Rohish Zade <zaderohish5@gmail.com>
2024-09-24 23:38:04 +0530

    test

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-# This is a test file
+def swap(x, y):
+    temp = x
+    x = y
+    y = temp
 
-import pandas as pd
-import sqlalchemy as sal
+# Driver code
+x = 2
+y = 3
+swap(x, y)
+print(x)
+print(y)
 
-connection_string = ('mssql://<sql server _name>\SQLEXPRESS/<database_name>?driver=ODBC+DRIVER+17+FOR+SQL+SERVER')
-engine = sal.create_engine(connection_string)
-conn=engine.connect()
-
-
-# reading from a table in sql server and storing in a df
-df_sql = pd.read_sql_query('select * from orders', conn)
-print(df_sql)
-
+5
+2 3 6 6 5
